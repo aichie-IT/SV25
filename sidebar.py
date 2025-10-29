5b17bd9c2127a37251bd43beb01137c5b18a09b9
aichie-IT <s22a0051@siswa.umk.edu.my>
2025-10-30 02:57:07 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -4,17 +4,6 @@ st.set_page_config(
     page_title="Motor Accident Severity"
 )
 
-# --- Sidebar Logo ---
-st.sidebar.markdown(
-    """
-    <div style='text-align:center; margin-bottom:15px;'>
-        <img src='https://cdn-icons-png.flaticon.com/512/743/743922.png' width='100'>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-
 visualise = st.Page('main.py', title='Motor Accident Severity Analysis', icon=":material/motor:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
