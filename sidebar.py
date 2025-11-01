4c0a664b9db90fc45e224739884c93e7b5bc1565
aichie-IT <s22a0051@siswa.umk.edu.my>
2025-11-01 19:18:25 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -3,19 +3,19 @@ import streamlit as st
 st.set_page_config(page_title="Motor Accident Severity", layout="wide")
 
 # Define Pages
-home = st.Page("home.py", title="🏠 Home", icon=":material/home:")
+home = st.Page("home.py", title="Home", icon=":material/home:")
 
-overview = st.Page("overview.py", title="⚙️ General Overview", icon=":material/insights:")
-factors = st.Page("factors.py", title="📊 Accident Factors", icon=":material/bar_chart:")
-numerical = st.Page("numerical.py", title="📈 Numerical Analysis", icon=":material/analytics:")
-advanced = st.Page("advanced.py", title="📉 Advanced Visualizations", icon=":material/show_chart:")
-correlation = st.Page("correlation.py", title="🗺️ Correlation Insights", icon=":material/share:")
-behavior = st.Page("behavior.py", title="🏍️ Riding Behavior Insights", icon=":material/pedal_bike:")
+overview = st.Page("overview.py", title="General Overview", icon=":material/insights:")
+factors = st.Page("factors.py", title="Accident Factors", icon=":material/bar_chart:")
+numerical = st.Page("numerical.py", title="Numerical Analysis", icon=":material/analytics:")
+advanced = st.Page("advanced.py", title="Advanced Visualizations", icon=":material/show_chart:")
+correlation = st.Page("correlation.py", title="Correlation Insights", icon=":material/share:")
+behavior = st.Page("behavior.py", title="Riding Behavior Insights", icon=":material/pedal_bike:")
 
 # Sidebar Navigation
 pg = st.navigation({
-    "🏠 Home": [home],
-    "🏍️ Motor Accident Severity Analysis": [
+    "Menu": [home],
+    "Motor Accident Severity Analysis": [
         overview,
         factors,
         numerical,
